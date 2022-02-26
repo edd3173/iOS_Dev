bb95578b317165a5bdbaf565fed72c92735b94e6
edd3173 <stargazers737@gmail.com>
2022-02-26 21:56:41 +0900

    Project Made

---
+++ I_Am_Rich/I_Am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_Rich
+//
+//  Created by 원성현 on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
