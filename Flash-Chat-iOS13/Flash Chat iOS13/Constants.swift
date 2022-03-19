f3d2e26649f6c1ec2321ed2948d4275eac4c5241
edd3173 <stargazers737@gmail.com>
2022-03-20 03:13:31 +0900

    ~188

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by 원성현 on 2022/03/20.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    static let appName = "⚡️FlashChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
